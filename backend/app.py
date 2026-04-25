4fb5ac6b84743a1922b8e3a20fc05a5f25c42591
yashvi <bhoyeyashvi@gmail.com>
2026-04-26 02:10:02 +0530

    Moved MongoDB URI to .env

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from pymongo import MongoClient
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -5,7 +8,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-client = MongoClient("mongodb://yashvi_test_user:yashvitest1@ac-do8yb4a-shard-00-00.l7xtbf4.mongodb.net:27017,ac-do8yb4a-shard-00-01.l7xtbf4.mongodb.net:27017,ac-do8yb4a-shard-00-02.l7xtbf4.mongodb.net:27017/?ssl=true&replicaSet=atlas-esz1l5-shard-0&authSource=admin&appName=Cloud-Cluster")
+client = MongoClient(os.getenv("MONGO_URI"))
 
 db = client["user_behavior_db"]
 collection = db["events"]
